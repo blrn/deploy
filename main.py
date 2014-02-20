329baf182f586b832427e05d5bbdb21016d48454
Tyler Cook <chef1991@gmail.com>
2014-02-20 04:02:36 -0600

    added logic for choosing which files to send in main.py

--- main.py
+++ main.py
@@ -3,9 +3,23 @@
 import args
 from config import Config
 from config import ConfigError
+import os
 
-batch_file_name  = ".deploy_batch"
+
 	
+def get_files(directory='.'):
+	file_dict = dict()
+	file_list = list()
+	for f in os.listdir(directory):
+		if os.path.isfile(f):
+			file_list.append(f)
+		elif os.path.isdir(f):
+			file_dict[f] = get_files(f)
+	if(directory is '.'):
+		file_dict['.'] = file_list
+		return file_dict
+	return file_list
+
 def handle_config_error(e):
 	if e.cause is ConfigError.INVALID_NAME_ERROR:
 		print "the supplied name is not valid."
@@ -42,6 +56,26 @@ def main():
 
 	# everthing is set up i think,
 	# implement sftp here
+	file_dict = get_files()
+	print file_dict
+	for directory in file_dict:
+		print directory
+		if config.ignore(directory):
+			del file_dict[directory]
+			continue
+		else:
+			i = 0
+			size = len(file_dict[directory])
+			while i < size:
+				file = file_dict[directory][i]
+				print file
+				if config.ignore(file):
+					print "ignore:", file
+					del file_dict[directory][i]
+					size -= 1
+				else:
+					i += 1
+	file_dict
 
 if __name__ == '__main__':
 	main()
