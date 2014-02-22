2bb7f2da3b221a67f09d574c388705e97de65dee
Tyler Cook <chef1991@gmail.com>
2014-02-22 00:33:05 -0600

    removed debug prints

--- sftp.py
+++ sftp.py
@@ -10,20 +10,12 @@ class Sftp(object):
 	def send_files(self, user, host):
 		self.user = user
 		self.host = host
-		print self.user
-		print self.host
 		batch_file = open(".deploy_batch", 'w')
 		batch_file.write("cd " + str(self.destination) + "\n")
 		self.__make_batch_file(batch_file)
 		batch_file.write("bye")
 		batch_file.close()
-		batch_file = open(".deploy_batch", "r")
-		print "batch_file:"
-		for line in batch_file:
-			print line.rstrip()
-		batch_file.close()
 		cmd = "sftp -b " + ".deploy_batch" + " " + user + "@" + host
-		print cmd
 		os.system(cmd)
 		os.remove(".deploy_batch")
 
@@ -39,11 +31,8 @@ class Sftp(object):
 			batch_file.write("! ssh " + self.user + "@" + self.host + " \"mkdir -p " + self.destination + "/" + directory +"\""+"\n" )
 			batch_file.write("cd " + directory[directory.rfind('/')+1:len(directory)] +"\n")
 			batch_file.write("lcd " + directory[directory.rfind('/')+1:len(directory)] +"\n")
-		#print "directory:", directory
-		#print "file_dict: ", file_dict
 		for f in file_dict[directory]:
 			if isinstance(f,dict):
-				#print "f:", f
 				dir_path = copy.deepcopy(f).popitem()[0]
 				self.__make_batch_file(batch_file, dir_path, file_dict=f)
 			else:
