a9d0fb170527162cb62775f85f95d772bb70e8d7
Tyler Cook <chef1991@gmail.com>
2014-02-22 05:21:09 -0600

    changed project structure for setuptools support

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import sys, os
+setup(name="deploy",
+	version="0.0.2a",
+	description="Python CLI application that transfers files to a remote web server.",
+	packages=find_packages(),
+	package_data = {
+		'deploy' : ['templates/*.json']
+	},
+	zip_safe=False,
+	entry_points = {
+		'console_scripts': [
+			'deploy = deploy.main:main'
+		]
+	}
+)
